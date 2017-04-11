a2a28bd4edd7be36c2d0d05e355d816e425efd1a
Julio Renner <julio.renner@hotmail.com>
2017-04-11 20:08:04 -0300

    Initial Commit

---
+++ applications_menu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  applications_menu
+//
+//  Created by Júlio Renner on 11/04/17.
+//  Copyright © 2017 RENNERJ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
